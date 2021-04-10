531c158c424cc0ce09f6222a278ecebe2af0fc99
Nikita <nikitashamkut@gmail.com>
2021-04-10 21:28:49 +0300

    feat: add algorithm

--- src/sagas/grill.js
+++ src/sagas/grill.js
@@ -1,13 +1,51 @@
-import { takeLatest, put } from 'redux-saga/effects'
+import { takeLatest, put, call, select } from 'redux-saga/effects'
+
+import GrillSortingAlgorithm from '@/utils/GrillSortingAlgorithm'
+import { grillConfig as userGrillConfig } from '@/store'
 
 import {
   GET_USER_INPUT_FOR_GRILL_CONFIG,
   setGrillConfigAction,
+  setFitsItemsAction,
+  setOutOfGrillItemsAction,
 } from '@/actions'
 
 export function* setGrillConfigWorker({ payload }) {
   try {
-    yield put(setGrillConfigAction(payload))
+    yield put(setGrillConfigAction(JSON.parse(payload)))
+    const grillConfig = yield select(userGrillConfig)
+
+    const packer = yield new GrillSortingAlgorithm(
+      grillConfig.grill.width,
+      grillConfig.grill.height
+    )
+    const parsedItems = yield call(
+      packer.parsItems,
+      grillConfig.grill.grillItems
+    )
+    yield packer.fit(parsedItems)
+    const fitsItems = yield []
+    const outOfGrillItems = yield []
+    for (var n = 0; n < parsedItems.length; n++) {
+      var item = parsedItems[n]
+      if (item.fit) {
+        fitsItems.push({
+          positionX: item.fit.x,
+          positionY: item.fit.y,
+          with: item.width,
+          height: item.height,
+          title: item.title,
+        })
+      } else {
+        outOfGrillItems.push({
+          width: item.width,
+          height: item.height,
+          title: item.title,
+        })
+      }
+    }
+    yield put(setFitsItemsAction(fitsItems))
+    yield put(setOutOfGrillItemsAction(outOfGrillItems))
   } catch (error) {}
 }
 
